c3bdf764d6a654e04efd0860bc02a990fd8c9b0b
boradibra <boradibra1@gmail.com>
2024-09-26 11:36:28 -0400

    Added SockResponse struct in entities.go for returning sock details and variants

--- api/types/entities.go
+++ api/types/entities.go
@@ -20,21 +20,27 @@ type Message struct {
 	Message string `json:"message"`
 }
 
-
 type Sock struct {
-    ID              *int       `json:"id,omitempty"`									                   
-    Name            string    `json:"name" validate:"required"`		
-    Description     *string    `json:"description,omitempty"`				
-    PreviewImageURL string    `json:"previewImageUrl" validate:"required"`	
-    CreatedAt       *time.Time `json:"createdAt,omitempty"`							
+	ID              *int       `json:"id,omitempty"`
+	Name            string     `json:"name" validate:"required"`
+	Description     *string    `json:"description,omitempty"`
+	PreviewImageURL string     `json:"previewImageUrl" validate:"required"`
+	CreatedAt       *time.Time `json:"createdAt,omitempty"`
 }
 
 type SockVariant struct {
-    ID        *int       `json:"id,omitempty"`											
-    SockID    *int       `json:"sockId,omitempty"`										
-    Size      string    `json:"size" validate:"required,oneof=S M LG XL"`	
-    Price     float64   `json:"price" validate:"required,gt=0,lt=101"`		
-    Quantity  int       `json:"quantity" validate:"required,gte=0"`	
-    CreatedAt *time.Time `json:"createdAt,omitempty"`
+	ID        *int       `json:"id,omitempty"`
+	SockID    *int       `json:"sockId,omitempty"`
+	Size      string     `json:"size" validate:"required,oneof=S M LG XL"`
+	Price     float64    `json:"price" validate:"required,gt=0,lt=101"`
+	Quantity  int        `json:"quantity" validate:"required,gte=0"`
+	CreatedAt *time.Time `json:"createdAt,omitempty"`
 }
 
+type SockResponse struct {
+	SockID          int           `json:"sock_id"`
+	Name            string        `json:"name"`
+	Description     string        `json:"description"`
+	PreviewImageURL string        `json:"preview_image_url"`
+	Variants        []SockVariant `json:"variants"`
+}
