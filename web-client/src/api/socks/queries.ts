1ae38f0ca08e985d12ef95758e564da8c63a2c5e
boradibra <boradibra1@gmail.com>
2024-10-26 16:31:13 -0400

    Updated queries.ts in socks

---
+++ web-client/src/api/socks/queries.ts
@@ -0,0 +1,18 @@
+import { UseQueryResult, queryOptions, useQuery } from '@tanstack/react-query';
+import { Sock } from './model';
+import { HttpSockService } from './service';
+
+const sockService = new HttpSockService();
+
+export function useGetSockByIdOptions(sockId?: number) {
+  return queryOptions({
+    queryKey: ['socks', { sockId }],
+    queryFn: () => sockService.getSockById(sockId!),
+    enabled: Boolean(sockId),
+  });
+}
+
+export function useGetSockById(sockId?: number): UseQueryResult<Sock> {
+  return useQuery(useGetSockByIdOptions(sockId));
+}
+
